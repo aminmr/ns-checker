1382b6afbddce2cf7813872023e8d403eb53e754
Amin Mohammadian <amin79mr@gmail.com>
2023-10-11 02:45:25 +0330

    checking ns existence with kubectl get ns command execution + fix no namespace found error

--- main.go
+++ main.go
@@ -2,14 +2,18 @@ package main
 
 import (
 	"bytes"
+	// "context"
 	"encoding/json"
-	"flag"
 	"fmt"
 	"log"
 	"os"
 	"os/exec"
+	"strings"
 
+	// "k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/clientcmd"
+	// metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	
 )
 
 // Namespace represents a Kubernetes namespace
@@ -47,7 +51,7 @@ func main() {
 		return
 	}
 
-	// Create Kubernetes client
+	// // Create Kubernetes client
 	// config, err := clientcmd.BuildConfigFromFlags("", kubeconfig)
 	// if err != nil {
 	// 	fmt.Println("Error getting kubeconfig:", err)
@@ -60,18 +64,50 @@ func main() {
 	// }
 
 	// Pass through the rest of the command to kubectl
-	flag.Parse()
-	args := flag.Args()
+	args := os.Args[1:]
+
 	cmd := exec.Command("kubectl", args...)
 
-	// Check if the command is 'kubectl create ns' or 'kubectl get ns', if so, ignore
-	if len(args) > 1 && ((args[0] == "create" && args[1] == "ns") || (args[0] == "get" && args[1] == "ns")) {
+	// // List namespaces
+	// namespaces, err := clientset.CoreV1().Namespaces().List(context.TODO(), metav1.ListOptions{})
+	// if err != nil {
+	// 	fmt.Println("It's not OK:", err)
+	// 	return
+	// }
+
+	// 	if namespaces != nil {
+	// 	fmt.Println("It's OK")
+	// } else {
+	// 	fmt.Println("It's not OK: No namespaces found")
+	// }
+
+	createNsPresent := false
+	getNsPresent := false
+
+	// Check for 'create ns' or 'get ns' in all positions of args
+	for i := 0; i < len(args)-1; i++ { // -1 to avoid index out of range on args[i+1]
+		if args[i] == "create" && args[i+1] == "ns" {
+			createNsPresent = true
+			break // Exit the loop if found
+		} else if args[i] == "get" && args[i+1] == "ns" {
+			getNsPresent = true
+			break // Exit the loop if found
+		}
+	}
+	if createNsPresent || getNsPresent {
 		// Execute the original kubectl command
 		cmd.Stdout = os.Stdout
-		cmd.Stderr = os.Stderr
+		var stderr bytes.Buffer
+		cmd.Stderr = &stderr
 		if err := cmd.Run(); err != nil {
-			fmt.Fprintln(os.Stderr, "Command failed:", err)
-			os.Exit(1)
+			// Check if the error message, status code, and reason match your criteria
+			errorOutput := stderr.String()
+			if strings.Contains(errorOutput, "no namespace is found") {
+				fmt.Print("You have no project. Please create a new project with kubectl create ns NAMESPACE_NAME")
+			} else {
+				fmt.Print("kubectl failed:", errorOutput)
+			}
+			return
 		}
 		return
 	} else {
@@ -80,11 +116,19 @@ func main() {
 			cmd := exec.Command("kubectl", "get", "ns", "-o", "json")
 			var out bytes.Buffer
 			cmd.Stdout = &out
+			var stderr bytes.Buffer
+			cmd.Stderr = &stderr
 			err := cmd.Run()
 			if err != nil {
-				log.Fatalf("Failed to get namespaces: %v", err)
+				// Check if the error message, status code, and reason match your criteria
+				errorOutput := stderr.String()
+				if strings.Contains(errorOutput, "no namespace is found") {
+					fmt.Print("You have no project. Please create a new project with kubectl create ns NAMESPACE_NAME")
+				} else {
+					fmt.Print("kubectl failed:", errorOutput)
+				}
+				return
 			}
-
 			// Parse the JSON output
 			var nsList NamespaceList
 			err = json.Unmarshal(out.Bytes(), &nsList)
@@ -93,10 +137,7 @@ func main() {
 			}
 
 			// Check if the namespace list is empty
-			if len(nsList.Items) == 0 {
-				fmt.Println("You have no project. Please create a new project with kubectl create ns NAMESPACE_NAME")
-				return
-			} else {
+			if len(nsList.Items) > 0 {
 				fmt.Println("You have projects in CaaS but you need to set in your kubeconfig")
 				return
 			}
